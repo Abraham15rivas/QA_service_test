517caae4e30d0aa6424a43c5d408c6c1f99f452d
Abraham15rivas <abraham15rivas@gmail.com>
2021-03-05 17:18:59 -0400

    initial setup

---
+++ database/migrations/2021_03_05_202039_create_events_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('description');
+            $table->string('image')->nullable();
+            $table->string('video')->nullable();
+            $table->boolean('qa')->default(true);
+            $table->boolean('status')->default(true);
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('SET NULL');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
--- database/migrations/2021_03_05_202112_create_questions_table.php
+++ database/migrations/2021_03_05_202112_create_questions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateQuestionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,21 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('questions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->foreignId('role_id')
+            $table->text('content');
+            $table->enum('status', ['revision', 'approved', 'denied']);
+            // pertenece a un user
+            $table->foreignId('user_id')
                 ->nullable()
                 ->constrained()
                 ->onUpdate('cascade')
+                ->onDelete('SET NULL');
+            // pertenece a un event
+            $table->foreignId('event_id')
+                ->constrained()
+                ->onUpdate('cascade')
                 ->onDelete('cascade');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -36,6 +39,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('questions');
     }
 }
